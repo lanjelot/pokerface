d90b826ca3aa6182aef60181756a2164428ea898
lanjelot <lanjelot@gmail.com>
2022-08-02 12:25:01 +1000

    Add OCR on button3

--- scraper.py
+++ scraper.py
@@ -6,7 +6,12 @@ import io
 import random
 import cv2
 import numpy
+import re
+from tesserocr import image_to_text, PyTessBaseAPI, PSM
+from hashlib import md5
 from PIL import Image
+from texasholdem import Card
+from texasholdem.evaluator import  evaluate, rank_to_string, get_five_card_rank_percentage
 
 CARD_VALUES = "23456789TJQKA"
 CARD_SUITS = "shdc"
@@ -59,6 +64,8 @@ REGION_BOARD3_SUIT = (1050, 470, 1095, 520)
 REGION_BOARD4_SUIT = (1175, 470, 1210, 520)
 REGION_BOARD5_SUIT = (1295, 470, 1330, 520)
 
+def hash_image(image):
+    return md5(image.tobytes()).hexdigest()
 
 def test_screencaps(d='/home/seb/screencaps-board-bak/'):
     for i, f in enumerate(sorted(os.listdir(d))):
@@ -131,9 +138,13 @@ def read_board(image):
 def chunk(s, bs):
   return [s[i:i + bs] for i in range(0, len(s), bs)]
 
-def pp_cards(cards, fmt='@@'):
-    s = ' '.join(chunk(cards, 2))
-    s = fmt.replace('@@', s) 
+def pp_cards(cards, board, stats):
+    s = ' '.join(re.sub('[\[\] ]', '', Card(c).pretty_string) for c in chunk(cards, 2))
+    if board:
+        s = s + '  %-14s' % ' '.join(re.sub('[\[\] ]', '', Card(c).pretty_string) for c in chunk(board, 2))
+    else:
+        s += ' ' * 16
+    s = s + ' - ' + stats
     print(s)
 
 SBAR_Y = 930
@@ -157,6 +168,15 @@ def how_strong(image):
             continue
     return strength
 
+def eval_rank(cards, board):
+    if not board:
+        return 0
+    e = evaluate(
+        cards = [Card(c) for c in chunk(cards, 2)],
+        board = [Card(c) for c in chunk(board, 2)]
+    )
+    return int(get_five_card_rank_percentage(e) * 100)
+
 XY_FOLD = (1000, 1000) # FOLD
 XY_CALL = (1500, 1000) # CHECK, CALL
 XY_BET = (2000, 1000) # BET, RAISE, ALL IN
@@ -167,7 +187,7 @@ def tap(xy):
     # fake_sleep(3)
     x, y = [str(n) for n in xy]
     _ = subprocess.check_output(['adb', 'shell', 'input', 'tap', x, y])
-    time.sleep(.5) # 
+    time.sleep(.2) #
 
 def do_allin():
     tap(XY_BET)
@@ -204,6 +224,10 @@ def can_act(image):
         image.getpixel((895, 1010))[0] > 200:
         return False
 
+    # hand stength meter
+    if image.getpixel((1760, 935)) != (132, 132, 138, 255):
+        return False
+
     # third button is BET, RAISE or ALL-IN
     if can_bet(image) or can_raise(image):
         # debug
@@ -212,17 +236,26 @@ def can_act(image):
 
     return False
 
+REGION_BUTTON3 = (1735, 960, 2145, 1059)
+TEMPLATES_BUTTON3 = {}
+for v in ['bet', 'raise', 'allin', 'callany']:
+    tv = cv2.imread('./cv/buttons/%s.png' % v, 0)
+    TEMPLATES_BUTTON3[v] = tv
+
+def read_button3(image):
+    return match_symbol(image, REGION_BUTTON3, TEMPLATES_BUTTON3)
+
 def can_bet(image):
-    return image.getpixel((1910, 1010)) == (255, 255, 255, 255)
+    return read_button3(image) == 'bet'
 
-def can_raise(image): # only matches RAISE or ALL-IN (but not CALL ANY)
-    return image.getpixel((1943, 1030)) == (255, 255, 255, 255)
+def can_raise(image):
+    return read_button3(image) == 'raise'
 
-def can_allin(image): # only matches ALL-IN (but not BET or RAISE)
-    return image.getpixel((1955, 1015)) == (255, 255, 255, 255)
+def can_allin(image):
+    return read_button3(image) == 'allin'
 
 def can_check(image):
-    return image.getpixel((1445, 1010)) == (255, 255, 255, 255)
+    return hash_image(image.crop((1440, 985, 1585, 1040))) == '5590b09a2a4c65a1e57aeb7a1d38d6b2'
 
 # def can_call(image):
 #     return not can_check(image) or can_allin(image)
@@ -233,10 +266,8 @@ def save_image(image):
         return
     image.save(filepath)
 
-BIG_BLIND = 10000
-
 def poll_table():
-
+    rank = 0
     while True:
         out = subprocess.check_output(['adb', 'exec-out', 'screencap', '-p'])
         image = Image.open(io.BytesIO(out))
@@ -248,117 +279,153 @@ def poll_table():
         if '??' in cards:
              continue
 
-        score = how_strong(image)
-        pp_cards(cards, '-> @@  %d/7' % score)
-
         board = read_board(image)
-        if board:
-            pp_cards(board)
+        score = how_strong(image)
+        
+        rank = eval_rank(cards, board)
+
+        pp_cards(cards, board, '%d/8 %d%%' % (score, rank))
 
         if score > 4:
             if can_allin(image):
-                if score in (6, 7):
+                if score >= 6:
                     do_allin()
                 else:
                     do_fold()
             else:
-                if score == 5:
-                    do_bet('POT')
-                elif score == 6:
-                    do_bet(BIG_BLIND * 5)
-                elif score == 7:
+                if score >= 6:
                     do_bet(BIG_BLIND * 10)
+                else:
+                    do_bet('POT')
 
         else:
             if can_check(image):
                 do_check()
             elif can_allin(image):
-                amount = read_mystack(image)
-                if amount <= BIG_BLIND * score:
-                    print('All in', amount)
+                mystack = read_mystack(image)
+                
+                if mystack <= BIG_BLIND * score:
+                    print('All in. %d <= BB*%d' % (mystack, score))
                     do_allin()
                 else:
                     do_fold()
             else: # CALL or RAISE
                 amount = read_call(image)
-                if amount <= BIG_BLIND * score-1:
-                    print('Called', amount)
+                if amount <= BIG_BLIND * (score-1):
+                    print('Call. %d <= BB*%d' % (amount, score-1))
                     do_call()
                 else:
                     do_fold()
 
         time.sleep(1) # 
 
+def read_number(img, region, fiddle=False):
+    for offset_x in range(4):
+        for offset_y in range(4):
+            r = (region[0]-offset_x, region[1]-offset_y, region[2]+offset_x, region[3]+offset_y)
+            image = img.crop(r).convert('L')
 
-REGION_MYSTACK_VALUE = (760, 915, 910, 945)
-REGION_POT_VALUE = (1015, 590, 1150, 625)
+            s = image_to_text(image, psm=7).rstrip() # PSM.SINGLE_BLOCK 6 PSM.SINGLE_LINE 7 PSM.SINGLE_WORD 8 PSM.SINGLE_CHAR 10
+            # api = PyTessBaseAPI()
+            # api.SetVariable("tessedit_char_whitelist", 'CALL 0123456789KM.,')
+            # # api.SetPageSegMode(PSM.SINGLE_LINE)
+            # api.SetImage(image)
+            # s = api.GetUTF8Text()
+            # debug
+            # image.save('/tmp/value.png')
+            # print('OCRed: %r' % s.strip())
 
-from tesserocr import PyTessBaseAPI, PSM, OEM, image_to_text
+            s = s.strip()
+            if s.startswith('CALL '):
+                s = s[5:]
 
-def read_number(image, region):
-    image = image.crop(region)
-    image = image.convert('L')
+            if re.match('[0-9,]+$', s):
+                s = s.replace(',', '')
+                return int(s)
+            elif re.match('[0-9.]+[KMB]$', s):
+                f = float(s[:-1])
+                if s.endswith('K'):
+                    f *= 1000
+                elif s.endswith('M'):
+                    f *= 1000000
+                elif s.endswith('B'):
+                    f *= 1000000000
+                return int(f)
+            elif not fiddle:
+                break
 
-    s = image_to_text(image, psm=7).rstrip() # PSM.SINGLE_BLOCK 6 PSM.SINGLE_LINE 7 PSM.SINGLE_WORD 8 PSM.SINGLE_CHAR 10
+    return None
 
-    # debug
-    image.save('/tmp/value.png')
-    print(s)
-
-    if ' ' in s:
-        _, s = s.split(' ')
-    s = s.replace(',', '')
-    
-    if s.endswith('K'):
-        s = s[:-1] + '000'
-    elif s.endswith('M'):
-        s = s[:-1] + '000000'
-    return int(s)
-
-def read_mystack(image):
-    return read_number(image, REGION_MYSTACK_VALUE)
-
-def read_pot(image):
-    return read_number(image, REGION_POT_VALUE)
-
-def read_call(image):
+def read_mystack(image, offset_x=-3, offset_x2=3):
     first = 0
     last = 0
-    for x in range(1350, 1700):
-        pixel = image.getpixel((x, 990))
-        if pixel == (255, 255, 255, 255):
-            if not first:
-                first = x
-            last = x
+    for x in range(765, 940):
+        for y in range(912, 944):
+            pixel = image.getpixel((x, y))
+            if pixel[0] > 100:
+                if first == 0:
+                    first = x
+                last = x
+    return read_number(image, (first+offset_x, 913, last+offset_x2, 944), True) 
+    # return read_number(image, (794, 913, 922, 944)) # n.mM OK /o/ y1:912-915 y2:940-944
 
-    return read_number(image, (first-5, 975, last+5, 1045)) # REGION_CALL_VALUE
+def read_pot(image):
+    return read_number(image, (1020, 590, 1170, 627))
+
+def read_call(image):
+    return read_number(image, (1350, 987, 1710, 1041), True)
 
 
-def test_call():
-    d = '/home/seb/calls'
-    for i, f in enumerate(sorted(os.listdir(d))):
-        image_path = os.path.join(d, f)
-        image = Image.open(image_path)
+# TESTS TESTS TESTS
 
-        read_call(image)
+EXPECTED_CALLS = [30000, 40000, 80000, 50000, 5000, 10000, 40000, 27260, 50450, 15700, 190000, 255000, 258230, 35000, 250000, 110000, 181830, 90000, 507000, 25000, 267850, 200000, 650000, 6160000, 100000, 1500000, 1000000, 200000, 600000]
+EXPECTED_MYSTACKS = [160450, 125000, 85000, 75000, 60000, 55000, 45000, 424750, 417736, 367286, 349650, 286334, 813884, 1100000, 656930, 1100000, 1000000, 157169, 4800000, 2000000, 1300000, 3700000, 2900000, 7100000, 5100000, 3900000, 1500000, 6300000, 13800000]
+EXPECTED_POTS = [50000, 40000, 160000, 40000, None, None, None, 25000, 30000, 130900, None, None, None, None, 450000, 120000, 40000, 180000, None, None, None, None, None, 750000, None, None, 5100000, None, 650000]
+def test_ocr():
+    dirpath = './tests/screencaps'
+    for i, filename in enumerate(sorted(os.listdir(dirpath))):
+        filepath = os.path.join(dirpath, filename)
+        image = Image.open(filepath)
+
+        actual = read_call(image)
+        expected = EXPECTED_CALLS[i]
+        if actual != expected:
+            print(actual, '!=', expected)
+
+        actual = read_mystack(image)
+        expected = EXPECTED_MYSTACKS[i]
+        if actual != expected:
+            print(actual, '!=', expected)
+
+        actual = read_pot(image)
+        expected = EXPECTED_POTS[i]
+        if actual != expected:
+            print(actual, '!=', expected)
 
 def blah():
-    d = '/home/seb/actions'
+    d = '/home/seb/actions-new'
     # xy = (1885, 1010) # button 3
     # xy = (1905, 1010) # button 3
-    xy = (1445, 1010) # button 2
+    # xy = (1445, 1010) # button 2
     for i, f in enumerate(sorted(os.listdir(d))):
         image_path = os.path.join(d, f)
         image = Image.open(image_path)
-        pixel = image.getpixel(xy)
-        print(pixel, image_path)
+        # pixel = image.getpixel(xy)
+        # print(pixel, image_path)
+        print(image_path, can_bet(image))
         # print('can_act:', can_act(image))
-        print('can_bet:', can_bet(image))
-        print('can_raise:', can_raise(image))
-        print('can_allin:', can_allin(image))
+        # print(
+        # print('can_raise:', can_raise(image))
+        # print('can_allin:', can_allin(image))
         # print('can_check:', can_check(image))
-        
 
+BIG_BLIND = 10000
+# ANDROID_SERIAL=
 if __name__ == '__main__':
+    from sys import argv, exit
+    if len(argv) != 2:
+        print('usage: <big blind>')
+        exit(2)
+    BIG_BLIND = int(argv[1])
     poll_table()
 
